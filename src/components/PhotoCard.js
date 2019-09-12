395157f09455f532bfea9a7703ef10e111df092b
Joe Denson <joedenson79@gmail.com>
2019-09-11 17:05:56 -0700

    fixed css mistake

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -4,9 +4,9 @@ import React from "react";
 const PhotoCard = props => {
   return (
     <div className="photo-card">
-      <h2>Title: {props.title}</h2>
+      <h2>{props.title}</h2>
       <img className="nasa-photo" alt="pic of the day" src={props.url} />
-      <h3>Explanation: {props.explanation}</h3>
+      <h3>{props.explanation}</h3>
       <h4>Copyright: {props.copyright}</h4>
     </div>
   );
